951c6e9efaa0fdfe971ab8e274014b300eca38e8
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-10 15:14:34 +0900

    change implementation of queue server

--- src/lmq_queue_server.erl
+++ src/lmq_queue_server.erl
@@ -2,6 +2,8 @@
 -behaviour(gen_server).
 -compile(export_all).
 
+-record(state, {refs=gb_sets:empty(), queue=queue:new()}).
+
 start() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -23,56 +25,71 @@ alive(UUID) ->
 init([]) ->
     lmq_queue:start(),
     process_flag(trap_exit, true),
-    {ok, []}.
+    {ok, #state{}}.
 
-handle_call({push, Data}, _From, Puller) ->
+handle_call({push, Data}, _From, S=#state{}) ->
     R = lmq_queue:enqueue(Data),
-    gen_server:cast(?MODULE, notify),
-    {reply, R, Puller};
-handle_call(pull, From, Puller) ->
-    case lmq_queue:dequeue() of
-        empty ->
-            Pid = spawn_link(?MODULE, delay_response, [self(), From]),
-            {noreply, [Pid|Puller]};
-        Message ->
-            {reply, Message, Puller}
+    case queue:is_empty(S#state.queue) of
+        true  -> {reply, R, S};
+        false -> {reply, R, S, lmq_queue:waittime()}
     end;
-handle_call({complete, UUID}, _From, Puller) ->
+handle_call(pull, From={Pid, _}, S=#state{refs=R, queue=Q}) ->
+    Ref = erlang:monitor(process, Pid),
+    NewState = S#state{refs=gb_sets:add(Ref, R), queue=queue:in({From, Ref}, Q)},
+    {noreply, NewState, lmq_queue:waittime()};
+handle_call({complete, UUID}, _From, S=#state{}) ->
     R = lmq_queue:complete(UUID),
-    {reply, R, Puller};
-handle_call({alive, UUID}, _From, Puller) ->
+    {reply, R, S};
+handle_call({alive, UUID}, _From, S=#state{}) ->
     R = lmq_queue:reset_timeout(UUID),
-    {reply, R, Puller};
-handle_call(stop, _From, Puller) ->
-    {stop, normal, ok, Puller}.
+    {reply, R, S};
+handle_call(stop, _From, S=#state{}) ->
+    {stop, normal, ok, S}.
 
-handle_cast(notify, Puller) ->
-    lists:foreach(fun(Pid) ->
-        Pid ! {lmq, self(), wake}
-    end, Puller),
-    {noreply, Puller}.
+handle_cast(Msg, S=#state{}) ->
+    io:format("Unknown message received: ~p~n", [Msg]),
+    {noreply, S}.
 
-handle_info({'EXIT', Pid, Reason}, Puller) ->
-    {noreply, Puller -- [Pid]};
-handle_info(Info, Puller) ->
-    io:format("unexpected message received: ~p~n", [Info]),
-    {noreply, Puller}.
+handle_info(timeout, S=#state{}) ->
+    NewState = maybe_push_message(S),
+    case queue:is_empty(NewState#state.queue) of
+        true  -> {noreply, NewState};
+        false -> {noreply, NewState, lmq_queue:waittime()}
+    end;
+handle_info({'DOWN', Ref, process, _Pid, _}, S=#state{refs=R, queue=Q}) ->
+    case gb_sets:is_member(Ref, R) of
+        true ->
+            erlang:demonitor(Ref, [flush]),
+            NewQueue = queue:filter(
+                fun({_, V}) when V =:= Ref -> false;
+                   (_) -> true
+                end, Q),
+            {noreply, S#state{refs=gb_sets:delete(Ref, R), queue=NewQueue}};
+        false ->
+            {noreply, S}
+    end;
+handle_info(Msg, S=#state{}) ->
+    io:format("Unknown message received: ~p~n", [Msg]),
+    {noreply, S}.
 
 terminate(_Reason, _State) ->
     ok.
 
-delay_response(SrvPid, From) ->
-    {Pid, _} = From,
-    link(Pid),
-    Timeout = lmq_queue:waittime(),
-    receive
-        {lmq, SrvPid, wake} -> ok
-    after Timeout ->
-        ok
-    end,
-    case lmq_queue:dequeue() of
-        empty -> delay_response(SrvPid, From);
-        Message ->
-            unlink(Pid),
-            gen_server:reply(From, Message)
+maybe_push_message(S=#state{refs=R, queue=Q}) ->
+    case queue:is_empty(Q) of
+        true -> S;
+        false ->
+            case lmq_queue:dequeue() of
+                empty -> S;
+                Msg ->
+                    case queue:out(Q) of
+                        {{value, {From, Ref}}, NewQueue} ->
+                            erlang:demonitor(Ref, [flush]),
+                            NewRefs = gb_sets:delete(Ref, R),
+                            gen_server:reply(From, Msg),
+                            S#state{refs=NewRefs, queue=NewQueue};
+                        {empty, Q} ->
+                            S
+                    end
+            end
     end.
