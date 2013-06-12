c5ff28c01194c2e07841b2bd616718de28885c15
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-12 19:47:08 +0900

    add tests

--- test/lmq_queue_SUITE.erl
+++ test/lmq_queue_SUITE.erl
@@ -2,11 +2,13 @@
 
 -include("lmq.hrl").
 -include_lib("common_test/include/ct.hrl").
--export([init_per_suite/1, end_per_suite/1, all/0]).
--export([test/1]).
+-export([init_per_suite/1, end_per_suite/1,
+    init_per_testcase/2, end_per_testcase/2,
+    all/0]).
+-export([push_pull_complete/1, return_to/1]).
 
 all() ->
-    [test].
+    [push_pull_complete, return_to].
 
 init_per_suite(Config) ->
     Priv = ?config(priv_dir, Config),
@@ -18,20 +20,37 @@ init_per_suite(Config) ->
 
 end_per_suite(_Config) ->
     application:stop(mnesia),
-    mnesia:delete_schema([node()]),
-    ok.
+    mnesia:delete_schema([node()]).
 
-test(_Config) ->
-    lmq_queue:start_link(),
-    lmq_lib:create(message),
-    ok = lmq_queue:push("foo"),
+init_per_testcase(_, Config) ->
+    {ok, Pid} = lmq_queue:start_link(),
+    ok = lmq_lib:create(message),
+    [{lmq, Pid} | Config].
+
+end_per_testcase(_, Config) ->
+    lmq_queue:stop(?config(lmq, Config)).
+
+push_pull_complete(_Config) ->
+    Ref = make_ref(),
+    ok = lmq_queue:push(Ref),
+    M = lmq_queue:pull(),
+    {TS, UUID} = M#message.id,
+    true = is_float(TS),
+    uuid:string_to_uuid(UUID),
+    Ref = M#message.data,
+    ok = lmq_queue:complete(UUID).
+
+return_to(_Config) ->
+    Ref = make_ref(),
+    ok = lmq_queue:push(Ref),
     M1 = lmq_queue:pull(),
+    Ref = M1#message.data,
     {_, UUID1} = M1#message.id,
     ok = lmq_queue:return(UUID1),
     not_found = lmq_queue:return(UUID1),
     not_found = lmq_queue:complete(UUID1),
     M2 = lmq_queue:pull(),
+    Ref = M2#message.data,
     {_, UUID2} = M2#message.id,
     true = UUID1 =/= UUID2,
-    ok = lmq_queue:complete(UUID2),
-    ok.
+    ok = lmq_queue:complete(UUID2).
