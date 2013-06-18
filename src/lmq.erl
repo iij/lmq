dc9df20a4560d3aa9d5385c9533a94f815b0aef5
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-18 17:13:50 +0900

    add stop function for test purpose

--- src/lmq.erl
+++ src/lmq.erl
@@ -1,7 +1,7 @@
 -module(lmq).
 
 -include("lmq.hrl").
--export([install/1, start/0]).
+-export([install/1, start/0, stop/0]).
 
 install(Nodes) ->
     ok = mnesia:create_schema(Nodes),
@@ -13,3 +13,8 @@ start() ->
     ok = application:start(mnesia),
     ok = application:start(ranch),
     ok = application:start(lmq).
+
+stop() ->
+    ok = application:stop(lmq),
+    ok = application:stop(ranch),
+    ok = application:stop(mnesia).
