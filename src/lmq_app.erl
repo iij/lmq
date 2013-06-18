55241e182db84924556227ceba01632331b9e45f
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-18 17:11:46 +0900

    fix application callbacks to shutdown correctly

--- src/lmq_app.erl
+++ src/lmq_app.erl
@@ -13,8 +13,9 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Port} = application:get_env(port),
-    lmq_sup:start_link(),
-    {ok, _} = msgpack_rpc_server:start(?MSGPACK_SERV, tcp, lmq_api, [{port, Port}]).
+    R = lmq_sup:start_link(),
+    {ok, _} = msgpack_rpc_server:start(?MSGPACK_SERV, tcp, lmq_api, [{port, Port}]),
+    R.
 
 stop(_State) ->
     msgpack_rpc_server:stop(?MSGPACK_SERV),
