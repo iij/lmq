9d795227f57b53194fd2c631030c1af29fb50fd1
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-19 20:30:18 +0900

    add lager

--- src/lmq.erl
+++ src/lmq.erl
@@ -3,7 +3,7 @@
 -include("lmq.hrl").
 -export([install/1, start/0, stop/0]).
 
--define(DEPS, [crypto, quickrand, uuid, msgpack, msgpack_rpc,
+-define(DEPS, [lager, crypto, quickrand, uuid, msgpack, msgpack_rpc,
     mnesia, ranch, lmq]).
 
 install(Nodes) ->
@@ -14,7 +14,16 @@ install(Nodes) ->
 
 start() ->
     [ensure_started(Dep) || Dep <- ?DEPS],
-    ok.
+
+    {ok, LagerConfig} = application:get_env(lmq, lager),
+    case proplists:get_value(handlers, LagerConfig) of
+        undefined -> ok;
+        Handlers ->
+            application:stop(lager),
+            application:load(lager),
+            application:set_env(lager, handlers, Handlers),
+            ensure_started(lager)
+    end.
 
 stop() ->
     [application:stop(Dep) || Dep <- lists:reverse(?DEPS)],
