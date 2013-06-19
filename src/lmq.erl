9a910640701fc2e90f7f69af6e7de73210f60e46
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-06-19 20:13:09 +0900

    fix app/release config

--- src/lmq.erl
+++ src/lmq.erl
@@ -3,6 +3,9 @@
 -include("lmq.hrl").
 -export([install/1, start/0, stop/0]).
 
+-define(DEPS, [crypto, quickrand, uuid, msgpack, msgpack_rpc,
+    mnesia, ranch, lmq]).
+
 install(Nodes) ->
     ok = mnesia:create_schema(Nodes),
     rpc:multicall(Nodes, application, start, [mnesia]),
@@ -10,11 +13,15 @@ install(Nodes) ->
     rpc:multicall(Nodes, application, stop, [mnesia]).
 
 start() ->
-    ok = application:start(mnesia),
-    ok = application:start(ranch),
-    ok = application:start(lmq).
+    [ensure_started(Dep) || Dep <- ?DEPS],
+    ok.
 
 stop() ->
-    ok = application:stop(lmq),
-    ok = application:stop(ranch),
-    ok = application:stop(mnesia).
+    [application:stop(Dep) || Dep <- lists:reverse(?DEPS)],
+    ok.
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok -> ok;
+        {error, {already_started, App}} -> ok
+    end.
