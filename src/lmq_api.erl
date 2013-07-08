6d385142a88da109e9293d387ebb9315bfd9582e
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-07-08 12:56:48 +0900

    add logging for api request

--- src/lmq_api.erl
+++ src/lmq_api.erl
@@ -3,21 +3,25 @@
 -export([create/1, push/2, pull/1, done/2, retain/2, release/2]).
 
 create(Name) when is_binary(Name) ->
+    lager:info("lmq_api:create(~s)", [Name]),
     Name1 = binary_to_atom(Name, latin1),
     ok = lmq_queue_mgr:create(Name1),
     <<"ok">>.
 
 push(Name, Msg) when is_binary(Name) ->
+    lager:info("lmq_api:push(~s, ~p)", [Name, Msg]),
     Pid = find(Name),
     lmq_queue:push(Pid, Msg),
     <<"ok">>.
 
 pull(Name) when is_binary(Name) ->
+    lager:info("lmq_api:pull(~s)", [Name]),
     Pid = find(Name),
     M = lmq_queue:pull(Pid),
     lmq_lib:export_message(M).
 
 done(Name, UUID) when is_binary(Name), is_binary(UUID) ->
+    lager:info("lmq_api:done(~s, ~s)", [Name, UUID]),
     Pid = find(Name),
     UUID1 = convert_uuid(UUID),
     case lmq_queue:done(Pid, UUID1) of
@@ -26,6 +30,7 @@ done(Name, UUID) when is_binary(Name), is_binary(UUID) ->
     end.
 
 retain(Name, UUID) when is_binary(Name), is_binary(UUID) ->
+    lager:info("lmq_api:retain(~s, ~s)", [Name, UUID]),
     Pid = find(Name),
     UUID1 = convert_uuid(UUID),
     case lmq_queue:retain(Pid, UUID1) of
@@ -34,6 +39,7 @@ retain(Name, UUID) when is_binary(Name), is_binary(UUID) ->
     end.
 
 release(Name, UUID) when is_binary(Name), is_binary(UUID) ->
+    lager:info("lmq_api:release(~s, ~s)", [Name, UUID]),
     Pid = find(Name),
     UUID1 = convert_uuid(UUID),
     case lmq_queue:release(Pid, UUID1) of
