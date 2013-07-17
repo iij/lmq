408781ab0b2bbdf86180ef0c6d8f1f6b82da4b9b
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-07-17 19:56:25 +0900

    change startup process

--- src/lmq_queue_mgr.erl
+++ src/lmq_queue_mgr.erl
@@ -1,20 +1,16 @@
 -module(lmq_queue_mgr).
 
 -behaviour(gen_server).
--export([start_link/1, queue_started/2, create/1, create/2, delete/1, find/1, match/1]).
+-export([start_link/0, queue_started/2, create/1, create/2, delete/1, find/1, match/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
     code_change/3, terminate/2]).
 
 -include("lmq.hrl").
--define(SPEC, {lmq_queue_sup,
-               {lmq_queue_sup, start_link, []},
-               permanent, 10000,
-               supervisor, [lmq_queue_sup]}).
 
 -record(state, {sup, qmap=dict:new()}).
 
-start_link(Sup) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, Sup, []).
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 queue_started(Name, QPid) when is_atom(Name) ->
     gen_server:cast(?MODULE, {queue_started, Name, QPid}).
@@ -34,9 +30,10 @@ match(Regexp) when is_list(Regexp) ->
 delete(Name) when is_atom(Name) ->
     gen_server:call(?MODULE, {delete, Name}).
 
-init(Sup) ->
-    %% start queue supervisor later in order to avoid deadlock
-    self() ! {start_queue_supervisor, Sup},
+init([]) ->
+    lists:foreach(fun(Name) ->
+        lmq_queue:start(Name)
+    end, lmq_lib:all_queue_names()),
     {ok, #state{}}.
 
 handle_call({create, Name, Props}, _From, S=#state{qmap=QMap}) when is_atom(Name) ->
@@ -88,12 +85,6 @@ handle_cast({queue_started, Name, QPid}, S=#state{qmap=QMap}) when is_atom(Name)
 handle_cast(_, State) ->
     {noreply, State}.
 
-handle_info({start_queue_supervisor, Sup}, S=#state{}) ->
-    {ok, Pid} = supervisor:start_child(Sup, ?SPEC),
-    lists:foreach(fun(Name) ->
-        lmq_queue:start(Name)
-    end, lmq_lib:all_queue_names()),
-    {noreply, S#state{sup=Pid}};
 handle_info({'DOWN', Ref, process, _Pid, _}, S=#state{qmap=QMap}) ->
     NewQMap = dict:filter(fun(_, {_, R}) ->
         R =/= Ref
