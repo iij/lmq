3aee4f6238a4c44cba9f4397dd03bbf133415279
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-07-18 14:50:25 +0900

    merge tests for lmq_misc into the module file

--- src/lmq_misc.erl
+++ src/lmq_misc.erl
@@ -4,3 +4,14 @@
 unixtime() ->
     {MegaSecs, Secs, MicroSecs} = os:timestamp(),
     MegaSecs * 1000000 + Secs + MicroSecs / 1000000.
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+unixtime_test() ->
+    T = lmq_misc:unixtime(),
+    ?assert(is_float(T)),
+    ?assertEqual(round(T) div 1000000000, 1).
+
+-endif.
