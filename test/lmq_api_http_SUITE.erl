b0fea8ccc58284a0082c4413bb549812df1870d0
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-10-18 16:33:54 +0900

    add test cases

---
+++ test/lmq_api_http_SUITE.erl
@@ -0,0 +1,47 @@
+-module(lmq_api_http_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-export([init_per_suite/1, end_per_suite/1,
+    init_per_testcase/2, end_per_testcase/2, all/0]).
+-export([push_pull/1]).
+
+-define(URL_QUEUE(Name), "http://localhost:8280/queues/" ++ Name).
+
+all() ->
+    [push_pull].
+
+init_per_suite(Config) ->
+    Priv = ?config(priv_dir, Config),
+    application:start(mnesia),
+    application:set_env(mnesia, dir, Priv),
+    lmq:start(),
+    ibrowse:start(),
+    Config.
+
+end_per_suite(_Config) ->
+    lmq:stop(),
+    mnesia:delete_schema([node()]).
+
+init_per_testcase(_, Config) ->
+    [{qname, "http_api_test"} | Config].
+
+end_per_testcase(_, Config) ->
+    Name = ?config(qname, Config),
+    lmq_queue_mgr:delete(list_to_atom(Name)).
+
+push_pull(Config) ->
+    Name = ?config(qname, Config),
+    {ok, "200", ResHdr, ResBody} = ibrowse:send_req(?URL_QUEUE(Name),
+        [{"content-type", "application/json"}],
+        post, "{\"message\":\"lmq test\"}"),
+    "application/json" = proplists:get_value("content-type", ResHdr),
+    "{\"packing\":\"no\"}" = ResBody,
+
+    {ok, "200", ResHdr2, ResBody2} = ibrowse:send_req(?URL_QUEUE(Name), [], get),
+    "application/json" = proplists:get_value("content-type", ResHdr2),
+    {Msg} = jsonx:decode(list_to_binary(ResBody2)),
+    true = list_to_binary(Name) =:= proplists:get_value(<<"queue">>, Msg),
+    true = is_binary(proplists:get_value(<<"id">>, Msg)),
+    <<"normal">> = proplists:get_value(<<"type">>, Msg),
+    <<"{\"message\":\"lmq test\"}">> = proplists:get_value(<<"content">>, Msg).
