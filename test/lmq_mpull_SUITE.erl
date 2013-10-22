3335493b31f14967e73a6ad0cab560aac08d505b
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-10-22 19:48:00 +0900

    add a test case

--- test/lmq_mpull_SUITE.erl
+++ test/lmq_mpull_SUITE.erl
@@ -4,10 +4,10 @@
 -include_lib("common_test/include/ct.hrl").
 -export([init_per_suite/1, end_per_suite/1,
     init_per_testcase/2, end_per_testcase/2, all/0]).
--export([client_closed/1]).
+-export([pull/1, client_closed/1]).
 
 all() ->
-    [client_closed].
+    [pull, client_closed].
 
 init_per_suite(Config) ->
     Priv = ?config(priv_dir, Config),
@@ -25,6 +25,20 @@ init_per_testcase(_, Config) ->
 
 end_per_testcase(_, _Config) ->
     ok.
+
+pull(_Config) ->
+    Queues = ['mpull/a', 'mpull/b'],
+    [lmq:update_props(Q, [{retry, 0}]) || Q <- Queues],
+    [lmq:push(Q, Q) || Q <- Queues],
+
+    Pids = [Pid || {ok, Pid} <- [lmq_mpull:start() || _ <- lists:seq(1, 4)]],
+    [{queue, R1}, _, _, _] = lmq_mpull:pull(lists:nth(1, Pids), <<"mpull/.*">>, 0),
+    [{queue, R2}, _, _, _] = lmq_mpull:pull(lists:nth(2, Pids), <<"mpull/.*">>, 0),
+    true = lists:sort([R1, R2]) =:= Queues,
+
+    empty = lmq_mpull:pull(lists:nth(3, Pids), <<"mpull/.*">>, 0),
+    empty = lmq_mpull:pull(lists:nth(4, Pids), <<"mpull/.*">>, 200),
+    [lmq:delete(Q) || Q <- Queues].
 
 client_closed(_Config) ->
     lmq:push('mpull/a', 1),
