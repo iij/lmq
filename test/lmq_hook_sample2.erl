a5995714fcc5426a9bf16c1c4be1aab9981ec49c
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2014-02-07 19:42:23 +0900

    unregister hooks that not included in new config

--- test/lmq_hook_sample2.erl
+++ test/lmq_hook_sample2.erl
@@ -2,13 +2,13 @@
 -behaviour(lmq_hook).
 
 -export([init/0, hooks/0, activate/1, deactivate/1]).
--export([custom_hook/2]).
+-export([custom_hook/2, custom_hook2/2]).
 
 init() ->
     ok.
 
 hooks() ->
-    [custom_hook].
+    [custom_hook, custom_hook2].
 
 activate([N, M]) ->
     {ok, N * M}.
@@ -19,3 +19,6 @@ deactivate(N) ->
 
 custom_hook(N, State) ->
     N * State.
+
+custom_hook2(N, State) ->
+    custom_hook(N, State).
