6799b50b3cc8a805739954e204cee3f89ffac1f1
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2015-02-06 13:40:32 +0900

    Fix histogram handling, close #45

--- src/lmq_cow_stats.erl
+++ src/lmq_cow_stats.erl
@@ -28,8 +28,10 @@ stats_to_json(Req, State) ->
     {jsonx:encode(R), Req, State}.
 
 jsonify_stats(Stats) ->
-    R = lists:foldr(fun({K, V}, Acc) when K =:= percentile orelse K =:= histogram ->
+    R = lists:foldr(fun({K, V}, Acc) when K =:= percentile ->
                             [{K, jsonify_proplist_keys(V)}|Acc];
+                       ({K, V}, Acc) when K =:= histogram ->
+                            [{K, jsonify_histogram(V)}|Acc];
                        (E, Acc) ->
                             [E|Acc]
                     end, [], proplists:get_value(retention, Stats)),
@@ -44,3 +46,101 @@ jsonify_proplist_keys([{K, V}|T], Acc) when is_integer(K) ->
     jsonify_proplist_keys(T, [{integer_to_binary(K), V}|Acc]);
 jsonify_proplist_keys([E|T], Acc) ->
     jsonify_proplist_keys(T, [E|Acc]).
+
+jsonify_histogram(L) ->
+    lists:foldr(fun({Bin, Count}, Acc) -> [[{bin, Bin}, {count, Count}]|Acc] end, [], L).
+
+%% ==================================================================
+%% EUnit test
+%% ==================================================================
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+jsonify_stats_test() ->
+    Value = [{push,
+              [{count,24},
+               {one,0.264930464299551},
+               {five,0.07182249856427589},
+               {fifteen,0.025679351706894803},
+               {day,2.7766597276742793e-4},
+               {mean,9.008392962075889e-8},
+               {acceleration,
+                [{one_to_five,6.436932191175838e-4},
+                 {five_to_fifteen,7.690524476230181e-5},
+                 {one_to_fifteen,2.658345695473958e-4}]}]},
+             {pull,
+              [{count,24},
+               {one,0.264930464299551},
+               {five,0.07182249856427589},
+               {fifteen,0.025679351706894803},
+               {day,2.7766597276742793e-4},
+               {mean,9.008394821789985e-8},
+               {acceleration,
+                [{one_to_five,6.436932191175838e-4},
+                 {five_to_fifteen,7.690524476230181e-5},
+                 {one_to_fifteen,2.658345695473958e-4}]}]},
+             {retention,
+              [{min,5.290508270263672e-4},
+               {max,0.0031280517578125},
+               {arithmetic_mean,0.0015397212084601907},
+               {geometric_mean,0.00127043818306925},
+               {harmonic_mean,0.0010519247023452565},
+               {median,0.0016698837280273438},
+               {variance,8.87850095456516e-7},
+               {standard_deviation,9.422579771254345e-4},
+               {skewness,0.43425944017223067},
+               {kurtosis,-1.3902812493309589},
+               {percentile,
+                [{50,0.0016698837280273438},
+                 {75,0.002238035202026367},
+                 {90,0.0029451847076416016},
+                 {95,0.0031011104583740234},
+                 {99,0.0031280517578125},
+                 {999,0.0031280517578125}]},
+               {histogram,[{0.0031280517578125,17}]},
+               {n,17}]}],
+    Expected = [{push,
+                 [{count,24},
+                  {one,0.264930464299551},
+                  {five,0.07182249856427589},
+                  {fifteen,0.025679351706894803},
+                  {day,2.7766597276742793e-4},
+                  {mean,9.008392962075889e-8},
+                  {acceleration,
+                   [{one_to_five,6.436932191175838e-4},
+                    {five_to_fifteen,7.690524476230181e-5},
+                    {one_to_fifteen,2.658345695473958e-4}]}]},
+                {pull,
+                 [{count,24},
+                  {one,0.264930464299551},
+                  {five,0.07182249856427589},
+                  {fifteen,0.025679351706894803},
+                  {day,2.7766597276742793e-4},
+                  {mean,9.008394821789985e-8},
+                  {acceleration,
+                   [{one_to_five,6.436932191175838e-4},
+                    {five_to_fifteen,7.690524476230181e-5},
+                    {one_to_fifteen,2.658345695473958e-4}]}]},
+                {retention,
+                 [{min,5.290508270263672e-4},
+                  {max,0.0031280517578125},
+                  {arithmetic_mean,0.0015397212084601907},
+                  {geometric_mean,0.00127043818306925},
+                  {harmonic_mean,0.0010519247023452565},
+                  {median,0.0016698837280273438},
+                  {variance,8.87850095456516e-7},
+                  {standard_deviation,9.422579771254345e-4},
+                  {skewness,0.43425944017223067},
+                  {kurtosis,-1.3902812493309589},
+                  {percentile,
+                   [{<<"50">>,0.0016698837280273438},
+                    {<<"75">>,0.002238035202026367},
+                    {<<"90">>,0.0029451847076416016},
+                    {<<"95">>,0.0031011104583740234},
+                    {<<"99">>,0.0031280517578125},
+                    {<<"999">>,0.0031280517578125}]},
+                  {histogram,[[{bin, 0.0031280517578125},{count,17}]]},
+                  {n,17}]}],
+    ?assertEqual(Expected, jsonify_stats(Value)).
+
+-endif.
